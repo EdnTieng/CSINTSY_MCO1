5a4b2687e7a6998e00aaaff21f628468de8d95f7
JohnathanTantanan <lance_xavier_lim@dlsu.edu.ph>
2025-06-27 20:23:37 +0800

    FINAL GRAPH

--- csintsy1/src/main/java/csintsy1/Graph.java
+++ csintsy1/src/main/java/csintsy1/Graph.java
@@ -28,46 +28,53 @@ public class Graph {
         }
         // Hardcoded weights and connections
         // Can add more nodes and change connections as needed
-        this.addEdge("S", "M", 9);
-        this.addEdge("S", "O", 6);
-        this.addEdge("S", "Q", 3);
-        this.addEdge("S", "R", 6);
-        this.addEdge("S", "T", 8);
+        this.addEdge("S", "M", 400);
+        this.addEdge("S", "O", 50);
+        this.addEdge("S", "Q", 50);
+        this.addEdge("S", "R", 160);
+        this.addEdge("S", "T", 500);
 
-        this.addEdge("M", "N", 1);
-        this.addEdge("M", "K", 1);
+        this.addEdge("M", "N", 60);
+        this.addEdge("M", "K", 190);
 
-        this.addEdge("N", "O", 1);
-        this.addEdge("N", "I", 1);
+        this.addEdge("N", "O", 210);
+        this.addEdge("N", "I", 90);
 
-        this.addEdge("O", "P", 1);
-        this.addEdge("O", "E", 1);
+        this.addEdge("O", "P", 110);
+        this.addEdge("O", "E", 280);
 
-        this.addEdge("P", "Q", 1);
-        this.addEdge("Q", "R", 1);
-        this.addEdge("Q", "C", 1);
+        this.addEdge("P", "Q", 225);
+        this.addEdge("Q", "R", 210);
+        this.addEdge("Q", "C", 280);
 
-        this.addEdge("R", "T", 1);
+        this.addEdge("R", "T", 260);
 
-        this.addEdge("T", "B", 1);
-        this.addEdge("T", "A", 1);
+        this.addEdge("T", "B", 190);
+        this.addEdge("T", "A", 160);
 
-        this.addEdge("K", "J", 1);
-        this.addEdge("J", "I", 1);
-        this.addEdge("I", "G", 1);
-        this.addEdge("G", "F", 1);
-        this.addEdge("F", "E", 1);
-        this.addEdge("E", "C", 1);
+        this.addEdge("K", "J", 20);
+        this.addEdge("J", "I", 10);
+        this.addEdge("I", "G", 50);
+        this.addEdge("G", "F", 50);
+        this.addEdge("F", "E", 30);
+        this.addEdge("E", "C", 230);
 
-        this.addEdge("C", "B", 1);
-        this.addEdge("C", "D", 1);
+        this.addEdge("C", "B", 60);
+        this.addEdge("C", "D", 280);
 
-        this.addEdge("U", "K", 1);
-        this.addEdge("U", "J", 1); 
+        this.addEdge("U", "K", 60);
+        this.addEdge("U", "V", 270); 
 
-        this.addEdge("J", "L", 1);
-        this.addEdge("L", "H", 1);
-        this.addEdge("H", "D", 1);   
+        this.addEdge("V", "L", 60);
+        this.addEdge("L", "H", 80);
+        this.addEdge("H", "D", 120);   
+
+        this.addEdge("I", "L", 170);
+        this.addEdge("K", "V", 50);
+        this.addEdge("J", "V", 50);
+        this.addEdge("G", "H", 120);
+        this.addEdge("F", "H", 170);
+        this.addEdge("E", "H", 180);
     }
 
     private void addEdge(String from, String to, int weight) {
